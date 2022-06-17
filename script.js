b055d2f0ce9fe7d4e0a9ecb44fc0ecf976cfa067
James Winston <kenjameswinston@gmail.com>
2022-06-17 18:32:58 -0400

    fixed logic bug

--- script.js
+++ script.js
@@ -4,93 +4,99 @@ roundNumber = 1;
 userWins = 0;
 computerWins = 0;
 
-
-
 function computerPlay() {
-    let plays = ['rock', 'paper', 'scissors'];
-    computerHand = plays[Math.round(Math.random() * (2 - 0) + 0)];
+  let plays = ["rock", "paper", "scissors"];
+  computerHand = plays[Math.round(Math.random() * (2 - 0) + 0)];
+  computerHand = toTitleCase(computerHand);
 
-    return computerHand;
+  return computerHand;
 }
 
 function playRound(computerSelection, playerSelection) {
-    let winner = '';
+    computerSelection = computerSelection.toLowerCase();
+    playerSelection = playerSelection.toLowerCase();
+  let winner = "";
 
-    if (computerSelection === 'rock') {
-        if (playerSelection === 'paper') {
-            winner = 'player';
-        }
-        else if (playerSelection === 'scissors') {
-            winner = 'computer';
-        }
-        else {
-            winner = 'tie';
-        }
+  if (computerSelection === "rock") {
+    if (playerSelection === "paper") {
+      winner = "player";
+    } else if (playerSelection === "scissors") {
+      winner = "computer";
+    } else {
+      winner = "tie";
     }
-    else if (computerSelection === 'paper') {
-        if (playerSelection === 'rock') {
-            winner = 'computer';
-        }
-        else if (playerSelection === 'scissors') {
-            winner = 'player';
-        } else {
-            winner = 'tie';
-        }  
+  } else if (computerSelection === "paper") {
+    if (playerSelection === "rock") {
+      winner = "computer";
+    } else if (playerSelection === "scissors") {
+      winner = "player";
+    } else {
+      winner = "tie";
     }
-
-    else if (computerSelection === 'scissors') {
-        if (playerSelection === 'rock') {
-            winner = 'player';
-        }
-        else if (playerSelection === 'paper') {
-            winner = 'computer';
-        } else {
-            return 'tie';
-        }  
-    
-    
+  } else if (computerSelection === "scissors") {
+    if (playerSelection === "rock") {
+      winner = "player";
+    } else if (playerSelection === "paper") {
+      winner = "computer";
+    } else {
+      return "tie";
     }
-    return winner;
+  }
+  return winner;
 }
+
+function toTitleCase(userString) {
+    userString = userString.toLowerCase()
+    firstLetter = userString.charAt(0)
+    firstLetterCap = firstLetter.toUpperCase();
+    titleCaseString = firstLetterCap + userString.slice(1);
+
+    return titleCaseString;
+}
+
+function game() {}
+
+
+
 //Main game loop
 while (stillPlaying) {
-    if (roundNumber > 5) {
-        if ( userWins > computerWins) {
-            console.log(`${computerWins} to ${userWins}', you win!`);
-        }
-        else if (userWins === computerWins) {
-            console.log("It's a tie.");
-        } else {
-            console.log(`${computerWins} to ${userWins}. You lost.`);
-        }
-
-        stillPlaying = false;
+  if (roundNumber > 5) {
+    if (userWins > computerWins) {
+      console.log(`You won the game!`);
+    } else if (userWins === computerWins) {
+      console.log("Tie Game.");
+    } else {
+      console.log(`You lost the game.`);
     }
+    console.log(`Final Score: ${userWins} to ${computerWins}`);
+    stillPlaying = false;
+    break;
+  }
+  
+  console.log(`ROUND: ${roundNumber}\n`)
+  userHand = prompt("What's your play? Type 'rock', 'paper', or 'scissors': ");
+  userHand = toTitleCase(userHand);
 
+  console.log(`${userHand}`)
 
-        userHand = prompt("What's your hand? Type 'rock', 'paper', or 'scissors': ");
-        computerHand = computerPlay();
+  computerHand = computerPlay();
+  console.log(`Computer: ${computerHand}`)
 
-        let theWinner = playRound(computerHand, userHand);
+  let theWinner = playRound(computerHand, userHand);
 
-        if (theWinner === 'player') {
-            console.log(`You:${userHand}`);
-            console.log(`Computer: ${computerHand}`);
-            console.log(`${theWinner} wins!`)
-        } else if (theWinner === 'computer') {
-            console.log(`You: ${userHand}`);
-            console.log(`Computer: ${computerHand}`);
-            console.log(`${theWinner} wins!`)
-        }
-
-        // If tie round, do the round over, don't increment it
-        if (theWinner === 'tie'){
-            console.log(`Tie round. Both players chose ${userHand}`);
-        }
-        else {
-            roundNumber ++;
-        }
-
+  if (theWinner === "player") {
+    userWins++;
+    console.log(`You win.`);
     
+  } else if (theWinner === "computer") {
+    computerWins++;
+    console.log(`-----COMPUTER WINS-----`);
+  }
 
+  // If tie round, do the round over, don't increment it
+  if (theWinner === "tie") {
+    console.log(`Tie round. Both players chose ${userHand}. Run it back!`);
+  } else {
+    roundNumber++;
+  }
 }
