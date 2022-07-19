c5fbd6c34d417822fda5f9beb7e008edf241ebff
j-winston <kenjameswinston@gmail.com>
2022-07-19 18:39:59 -0400

    hands now change and can play a single round

--- script.js
+++ script.js
@@ -1,49 +1,55 @@
-// stillPlaying = true;
-// roundNumber = 1;
+let stillPlaying = true;
+let roundNumber = 1;
 
-// userWins = 0;
-// computerWins = 0;
+let userWins = 0;
+let computerWins = 0;
 
-// function computerPlay() {
-//   let plays = ["rock", "paper", "scissors"];
-//   computerHand = plays[Math.round(Math.random() * (2 - 0) + 0)];
-//   computerHand = toTitleCase(computerHand);
 
-//   return computerHand;
-// }
 
-// function playRound(computerSelection, playerSelection) {
-//     computerSelection = computerSelection.toLowerCase();
-//     playerSelection = playerSelection.toLowerCase();
-//   let winner = "";
+function generateRandomHand() {
+  let plays = ["rock", "paper", "scissors"];
+  computerHand = plays[Math.round(Math.random() * (2 - 0) + 0)];
+  // computerHand = toTitleCase(computerHand);
 
-//   if (computerSelection === "rock") {
-//     if (playerSelection === "paper") {
-//       winner = "player";
-//     } else if (playerSelection === "scissors") {
-//       winner = "computer";
-//     } else {
-//       winner = "tie";
-//     }
-//   } else if (computerSelection === "paper") {
-//     if (playerSelection === "rock") {
-//       winner = "computer";
-//     } else if (playerSelection === "scissors") {
-//       winner = "player";
-//     } else {
-//       winner = "tie";
-//     }
-//   } else if (computerSelection === "scissors") {
-//     if (playerSelection === "rock") {
-//       winner = "player";
-//     } else if (playerSelection === "paper") {
-//       winner = "computer";
-//     } else {
-//       return "tie";
-//     }
-//   }
-//   return winner;
-// }
+  return computerHand;
+}
+
+function playRound() {
+  const computersHand = generateRandomHand();
+  changeComputerHand(computersHand)
+
+  const playersHand = document.querySelector('.active-hand').getAttribute('id')
+  
+  let winner = "";
+
+
+  if (computersHand === "rock") {
+    if (playersHand === "paper") {
+      winner = "player";
+    } else if (playersHand === "scissors") {
+      winner = "computer";
+    } else {
+      winner = "tie";
+    }
+  } else if (computersHand === "paper") {
+    if (playersHand === "rock") {
+      winner = "computer";
+    } else if (playersHand === "scissors") {
+      winner = "player";
+    } else {
+      winner = "tie";
+    }
+  } else if (computersHand === "scissors") {
+    if (playersHand === "rock") {
+      winner = "player";
+    } else if (playersHand === "paper") {
+      winner = "computer";
+    } else {
+      return "tie";
+    }
+  }
+  console.log(winner)
+}
 
 // function toTitleCase(userString) {
 //     userString = userString.toLowerCase()
@@ -54,10 +60,14 @@
 //     return titleCaseString;
 // }
 
-// //TODO--1. play a single round with 4 functioning buttons: rock, scissors, paper, and strike. Output results to console. Use background image 
 
-// //Main game loop
+
+//TODO--1. play a single round with 4 functioning buttons: rock, scissors, paper, and strike. Output results to console. Use background image 
+
+//Main game loop
+
 // while (stillPlaying) {
+//     // Decide who wins over the course of 5 rounds
 //   if (roundNumber > 5) {
 //     if (userWins > computerWins) {
 //       console.log(`You won the game!`);
@@ -70,31 +80,45 @@
 //     stillPlaying = false;
 //     break;
 //   }
+
+function changePlayerHand() {
+  const playerHandGraphic = document.querySelector('.player-hand');
+  const newHandId = this.getAttribute('id');
+
+  playerHandGraphic.src = `./assets/images/player/player-${newHandId}.png`;
+  playerHandGraphic.classList.add('active-hand')
+  playerHandGraphic.id = newHandId;
+}
+
+function changeComputerHand(hand) {
+  const computerHandGraphic = document.querySelector('.computer-hand');
+  computerHandGraphic.src = `./assets/images/computer/computer-${hand}.png`;
+ 
+}
+
+
+// Add event listeners for action buttons
+const buttons = document.querySelectorAll(".buttons");
+buttons.forEach(button => button.addEventListener('click', changePlayerHand))
+
+const strikeButton = document.querySelector('.strike-button');
+strikeButton.addEventListener('click', playRound)
   
-//   console.log(`ROUND: ${roundNumber}\n`)
-//   userHand = prompt("What's your play? Type 'rock', 'paper', or 'scissors': ");
-//   userHand = toTitleCase(userHand);
 
-//   console.log(`${userHand}`)
 
-//   computerHand = computerPlay();
-//   console.log(`Computer: ${computerHand}`)
-
-//   let theWinner = playRound(computerHand, userHand);
-
-//   if (theWinner === "player") {
-//     userWins++;
-//     console.log(`You win.`);
+  // if (theWinner === "player") {
+  //   userWins++;
+  //   console.log(`You win.`);
     
-//   } else if (theWinner === "computer") {
-//     computerWins++;
-//     console.log(`-----COMPUTER WINS-----`);
-//   }
+  // } else if (theWinner === "computer") {
+  //   computerWins++;
+  //   console.log(`-----COMPUTER WINS-----`);
+  // }
 
-//   // If tie round, do the round over, don't increment it
-//   if (theWinner === "tie") {
-//     console.log(`Tie round. Both players chose ${userHand}. Run it back!`);
-//   } else {
-//     roundNumber++;
-//   }
-// }//end while 
+  // // If tie round, do the round over, don't increment it
+  // if (theWinner === "tie") {
+  //   console.log(`Tie round. Both players chose ${userHand}. Run it back!`);
+  // } else {
+  //   roundNumber++;
+  // }
+//end while 
