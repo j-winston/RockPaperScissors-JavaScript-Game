08e3b80f994880dabc7bb175dd4f29eff69861a7
j-winston <kenjameswinston@gmail.com>
2022-08-11 14:57:12 -0400

    bug fix

--- script.js
+++ script.js
@@ -754,7 +754,6 @@ function sfxBox(){
 
 // If mobile device, change up the UI 
 if (window.matchMedia("(max-width: 400px)").matches) {
-  alert('mobile');
   document.querySelector('.rock-button').textContent = "R";
   document.querySelector('.paper-button').textContent = "P";
   document.querySelector('.scissors-button').textContent = "S";
